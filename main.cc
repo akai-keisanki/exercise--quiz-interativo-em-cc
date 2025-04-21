34794df9322bdc37a747828ab260bcb604c93a47
akai-keisanki <iago.ad@outlook.com>
2025-04-21 19:55:09 -0300

    feat(UserInterface): finished first version of the interface

--- main.cc
+++ main.cc
@@ -3,12 +3,14 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 
 #include <array>
 #include <vector>
-#include <list>
 
-class Program
+// Program class
+
+class QuizProgram
 {
 protected:
 
@@ -49,7 +51,7 @@ protected:
     /* Data storage */
 
     std::vector<Pergunta> perguntas;
-    std::list<Jogador> ranking;
+    std::vector<Jogador> ranking;
 
     /* Disk data manipulation functions */
     
@@ -75,7 +77,7 @@ protected:
         {
           if (!std::getline(perguntas_txt, stmp)) return 1;
           s.assign(stmp);
-        }
+          }
 
         std::getline(perguntas_txt, stmp);
         p.resposta = stmp[0] - 'A';
@@ -146,17 +148,16 @@ protected:
 
   class UserInterface
   {
-  protected:
-    
-    size_t points;
-
   public:
 
-    signed perguntar (const size_t& i, const Data::Pergunta& p)
+    char perguntar (const size_t& i, const Data::Pergunta& p)
     { 
       /* Show question interface for question `i` */
 
+      // Variables
       char ans;
+
+      // Question output
 
       std::cout << "Pergunta " << i << ':' << std::endl ;
 
@@ -165,27 +166,65 @@ protected:
       for (const std::string& s : p.questoes)
         std::cout << s << std::endl;
 
+      // Answer input
+
       std::cout << "Digite sua resposta (A-D): ";
 
       if (!(std::cin >> ans)) return -1;
 
-      return ans - 'A';
+      while (true)
+      {
+        if (ans > 'A' && ans < 'D')
+        {
+          ans -= 'A';
+          break;
+        }
+        if (ans > 'a' && ans < 'd')
+        {
+          ans -= 'a';
+          break;
+        }
+
+        std::cout << "Opcao invalida. Digite novamente (A-D): ";
+        if (!(std::cin >> ans)) return -1;
+      }
+
+      // Return of answer
+      return ans;
     }
 
-    signed fim (void)
+    std::string fim (const size_t& points, const size_t& total)
     {
       /* Show ending interface */
 
-      // TODO: Build the end interface
+      // Variables
+      std::string s;
 
-      return 0;
+      // Ending output
+
+      std::cout << "Quiz finalizado!" << std::endl
+        << "Voce acertou " << points << " de " << total << " perguntas." << std::endl << std::endl
+        << "Digite seu primeiro nome: ";
+
+      // Name input
+
+      std::cin >> s;
+
+      // Return of name
+      return s;
     }
 
-    void ranking (void)
+    void ranking (std::vector<Data::Jogador> ranking)
     {
       /* Show ranking interface */
 
-      // TODO: Show ranking
+      // Ranking title
+      std::cout << "--- Ranking ---" << std::endl;
+
+      // Ranking
+
+      for (const Data::Jogador& j : ranking)
+        std::cout << j.nome << ' ' << j.pontuacao << std::endl;
 
       return;
     }
@@ -194,21 +233,62 @@ protected:
 
 public:
 
-  Program () {}
+  QuizProgram () {}
 
   signed run_user_interface (void)
   {
     /* Main interface program */
 
-    // TODO: Run interface
+    // Variables
+    size_t points = 0;
+    char ans;
 
+    // Ask the questions and score
+
+    for (size_t i = 0; i < data.perguntas.size(); i ++)
+    {
+      ans = user_interface.perguntar(i, data.perguntas[i]);
+
+      if (ans < 0)
+      {
+        std::cerr << "Erro: Não foi possível obter a resposta.";
+        return -1;
+      }
+
+      // Pontuação
+      
+      if (ans == data.perguntas[i].resposta) points ++;
+    }
+
+    // End
+
+    Data::Jogador j {user_interface.fim(points, data.perguntas.size()), points};
+
+    std::vector<Data::Jogador>::iterator it = std::find_if(data.ranking.begin(), data.ranking.end(), [j] (const Data::Jogador& ji) { return ji.nome == j.nome; });
+
+    if (it == data.ranking.end())
+    {
+      data.ranking.push_back(j);
+    }
+    else
+    {
+      it->pontuacao = j.pontuacao;
+    }
+
+    // Preparando ranking
+
+    std::sort(data.ranking.begin(), data.ranking.end(), [] (const Data::Jogador& j1, const Data::Jogador& j2) { return j1.pontuacao > j2.pontuacao; });
+
+    // Sucessful return
     return 0;
   }
 };
+
+// Main function
 
 signed main (void)
 {
   /* Main program function */
 
-  return Program().run_user_interface();
+  return QuizProgram().run_user_interface();
 }
